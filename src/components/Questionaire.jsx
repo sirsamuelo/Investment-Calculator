cc7d824a26f63bf798ad78daadab447a0fcafe4d
Samuel Sirotnik <samec070@gmail.com>
2022-11-20 23:19:52 +0100

    praca na dotazniku

--- src/components/Questionaire.jsx
+++ src/components/Questionaire.jsx
@@ -3,22 +3,26 @@ import dotaznik from '../sample.js'
 import Checkbox from './Checkbox.jsx'
 
 const Questionaire = () => {
-    function findLengthOfCheckboxes(dotaznik) {
-        let sum = 0;
-        dotaznik.forEach((item) => {
-            sum = sum + Object.keys(item).length - 1
-        })
-        return sum
-    }
-    const sum = findLengthOfCheckboxes(dotaznik)
-    const [isChecked, setIsChecked] = useState(
-        new Array(sum).fill(false)
-      );
+  const [isChecked, setIsChecked] = useState(dotaznik)
 
   const onChange = (position) => {
-    const updateCheckedState = isChecked.map((item,index) => index === position ? !item : item)
-
+    const updateCheckedState = isChecked.map((item, index) => {
+      if (position === index) {
+        const d = !item.selected
+        return { ...item, selected: d }
+      } else {
+        return { ...item }
+      }
+    })
     setIsChecked(updateCheckedState)
+  }
+
+  const goOver = (index) => {
+    for (let i = 0; i < isChecked.length; i++) {
+      if (isChecked[index].selected === true) {
+        return true
+      }
+    }
   }
 
   const handleSubmit = (e) => {
@@ -27,27 +31,78 @@ const Questionaire = () => {
     console.log('submit')
   }
 
-  
+  function onClick() {
+    const updateCheckedState = isChecked.map((item) => {
+      return { ...item, selected: false }
+    })
+
+    setIsChecked(updateCheckedState)
+  }
+  function mojaFunkciakurva(position) {
+    isChecked.forEach((item, index) => {
+      if (position === index) {
+        const boolValueInObject = item.selected
+        return boolValueInObject
+      }
+    })
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <div className='questions'>
-            {/* change index somehoow  */}
-          {dotaznik.map(({question,a,b,c,d}, index) => {
+          {dotaznik.map((itemInDotaznik, index) => {
+            const { a, b, c, d, e, question, selected } = itemInDotaznik
+            console.log(selected)
             return (
               <div key={index} className='question__container'>
                 <div className='question'>{question}</div>
-                <Checkbox label={a} value={isChecked} onChange={() => onChange(index)}/>
-                <Checkbox label={b} value={isChecked} onChange={() => onChange(index)}/>
-                {c && <Checkbox label={c} value={isChecked} onChange={() => onChange(index)}/>}
-                {d && <Checkbox label={d} value={isChecked} onChange={() => onChange(index)}/>}
+                <Checkbox
+                  label={a}
+                  checked={mojaFunkciakurva(index)}
+                  onChange={() => onChange(index)}
+                  // disabled={goOver(index)}
+                />
+                <Checkbox
+                  label={b}
+                  checked={mojaFunkciakurva(index)}
+                  onChange={() => onChange(index)}
+                  // disabled={goOver(index)}
+                />
+                {c && (
+                  <Checkbox
+                    label={c}
+                    checked={mojaFunkciakurva(index)}
+                    onChange={() => onChange(index)}
+                    // disabled={goOver(index)}
+                  />
+                )}
+                {d && (
+                  <Checkbox
+                    label={d}
+                    checked={mojaFunkciakurva(index)}
+                    onChange={() => onChange(index)}
+                    // disabled={goOver(index)}
+                  />
+                )}
+
+                {e && (
+                  <Checkbox
+                    label={e}
+                    checked={mojaFunkciakurva(index)}
+                    onChange={() => onChange(index)}
+                    // disabled={goOver(index)}
+                  />
+                )}
               </div>
             )
-          })}{' '}
+          })}
         </div>
         <button className='btn-submit' type='submit'>
           Save
+        </button>
+        <button className='btn-clear' type='submit' onClick={onClick}>
+          Clear
         </button>
       </form>
     </div>
