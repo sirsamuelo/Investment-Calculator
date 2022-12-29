7b06b7c17ac623020c6a3c730655c2a485779c83
Samuel Sirotnik <samec070@gmail.com>
2022-12-29 17:52:25 +0100

    changed readme

--- src/components/Questionaire.jsx
+++ src/components/Questionaire.jsx
@@ -1,37 +1,47 @@
-import { useState, useEffect,useReducer } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import dotaznik from '../sample.js';
 import Checkbox from './Checkbox.jsx';
-import TransitionsModal from './TransitionsModal.jsx'
-import { Line} from 'rc-progress';
+import TransitionsModal from './TransitionsModal.jsx';
+import { Line } from 'rc-progress';
 
 const Questionaire = () => {
 	const [isChecked, setIsChecked] = useState([]);
-	const [count,setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
 	const initialState = {
-		slideIndex : 0,
+		slideIndex: 0,
 		submited: false,
-	}
+		isChecked: dotaznik
+	};
 
-	const questionsReducer = (state,event) => {
-		if(event.type === 'NEXT') {
-			if(state.slideIndex === 9) {
-				return {...state,submited: true}
-			};
+	const questionsReducer = (state, event) => {
+		if (event.type === 'CHECK') {
+			// if (state.slideIndex === 9) {
+			// 	return { ...state, submited: true };
+			// }
 			return {
 				...state,
-				slideIndex: state.slideIndex + 1
-			}
+				slideIndex: 0
+			};
 		}
-		if(event.type === 'OPEN') {
+		if (event.type === 'NEXT') {
+			if (state.slideIndex === 9) {
+				return { ...state, submited: true };
+			}
 			return {
-				...state, opened: true
-			}
+				...state,
+				slideIndex: state.slideIndex + 1,
+			};
 		}
-	}
+		if (event.type === 'OPEN') {
+			return {
+				...state,
+				opened: true,
+			};
+		}
+	};
 
-	const [state,dispatch] = useReducer(questionsReducer, initialState)
-
+	const [state, dispatch] = useReducer(questionsReducer, initialState);
 
 	useEffect(() => {
 		setIsChecked(dotaznik);
@@ -39,20 +49,20 @@ const Questionaire = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('submited')
-		setCount(getExpectedReturn())
+		console.log('submited');
+		setCount(getExpectedReturn());
 	};
 
 	function getExpectedReturn() {
-		var count = 0
-		for(let i = 0;i< isChecked.length;i++){
-			for(let j =0;j<isChecked[i].moznosti.length;j++) {
-				if(isChecked[i].moznosti[j].selected === true){
-					count = count + isChecked[i].moznosti[j].value 
+		var count = 0;
+		for (let i = 0; i < isChecked.length; i++) {
+			for (let j = 0; j < isChecked[i].moznosti.length; j++) {
+				if (isChecked[i].moznosti[j].selected === true) {
+					count = count + isChecked[i].moznosti[j].value;
 				}
 			}
 		}
-		return count
+		return count;
 	}
 
 	function myCheckboxFunction(e, id, indexInArray) {
@@ -60,7 +70,7 @@ const Questionaire = () => {
 		if (checked) {
 			const updateCheckedState = isChecked.map((item, index) => {
 				if (indexInArray === index) {
-					item.selected = true
+					item.selected = true;
 					let poleMoznosti = item.moznosti;
 					poleMoznosti.forEach((moznost) => {
 						if (moznost.id === id) {
@@ -76,7 +86,7 @@ const Questionaire = () => {
 			const updateCheckedState = isChecked.map((item, index) => {
 				if (indexInArray === index) {
 					let poleMoznosti = item.moznosti;
-					item.selected = false
+					item.selected = false;
 					poleMoznosti.forEach((moznost) => {
 						if (moznost.id === id) {
 							moznost.selected = false;
@@ -89,56 +99,108 @@ const Questionaire = () => {
 			setIsChecked(updateCheckedState);
 		}
 	}
-	function functionToDisable(moznostId,otazkaId) {
-		for(let i =0;i<isChecked.length;i++) {
-			if(i === otazkaId && isChecked[i].selected === true) {
-				let [a,b,c,d,e] = isChecked[i].moznosti
-				switch(moznostId){
+	function functionToDisable(moznostId, otazkaId) {
+		for (let i = 0; i < isChecked.length; i++) {
+			if (i === otazkaId && isChecked[i].selected === true) {
+				let [a, b, c, d, e] = isChecked[i].moznosti;
+				switch (moznostId) {
 					case 1:
-						return !a.selected
+						return !a.selected;
 					case 2:
-						return !b.selected
+						return !b.selected;
 					case 3:
-						return !c.selected
+						return !c.selected;
 					case 4:
-						return !d.selected
+						return !d.selected;
 					case 5:
-						return !e.selected
-					default: 
-						return false
+						return !e.selected;
+					default:
+						return false;
 				}
 			}
 		}
 	}
+
+	function getCount(){
+		let count = 0;
+		for (let i = 0; i < isChecked.length; i++) {
+			if(isChecked[i].selected === true) {
+				count++
+			}
+		}
+		console.log(count)
+		if(count < 10 ) {
+			return true
+		} 
+		return false;
+	}
 	return (
 		<div>
 			<form onSubmit={handleSubmit}>
-				<div className="questions__container">
-					{
-						isChecked && isChecked.map((item,index) => {
-							if(index === state.slideIndex) {
-								const {question,moznosti} = item
-								let [a,b,c,d,e] = moznosti
-								return <div key={index}>
-											<Line percent={(state.slideIndex + 1) * 10} strokeWidth={1} strokeColor="#0065ad" />
-											<div className='questionHeading'>{question}</div>
-											<div className="moznosti">
-												<Checkbox label={a.a} onChange={(e) => myCheckboxFunction(e, a.id, index)} disabled={functionToDisable(a.id,index)} />
-												<Checkbox label={b.b} onChange={(e) => myCheckboxFunction(e, b.id, index)} disabled={functionToDisable(b.id,index)} />
-												{c && <Checkbox label={c.c} onChange={(e) => myCheckboxFunction(e, c.id, index)} disabled={functionToDisable(c.id,index)} />}
-												{d && <Checkbox label={d.d} onChange={(e) => myCheckboxFunction(e, d.id, index)} disabled={functionToDisable(d.id,index)} />}
-												{e && <Checkbox label={e.e} onChange={(e) => myCheckboxFunction(e, e.id, index)} disabled={functionToDisable(e.id,index)} />}
-											</div>
+				<div className='questions__container'>
+					{isChecked &&
+						isChecked.map((item, index) => {
+							if (index === state.slideIndex) {
+								const { question, moznosti } = item;
+								let [a, b, c, d, e] = moznosti;
+								return (
+									<div key={index}>
+										<Line
+											percent={(state.slideIndex + 1) * 10}
+											strokeWidth={1}
+											strokeColor='#0065ad'
+										/>
+										<div className='questionHeading'>{question}</div>
+										<div className='moznosti'>
+											<Checkbox
+												label={a.a}
+												onChange={(e) =>  myCheckboxFunction(e, b.id, index)}
+												disabled={functionToDisable(a.id, index)}
+											/>
+											<Checkbox
+												label={b.b}
+												onChange={(e) => myCheckboxFunction(e, b.id, index)}
+												disabled={functionToDisable(b.id, index)}
+											/>
+											{c && (
+												<Checkbox
+													label={c.c}
+													onChange={(e) => myCheckboxFunction(e, c.id, index)}
+													disabled={functionToDisable(c.id, index)}
+												/>
+											)}
+											{d && (
+												<Checkbox
+													label={d.d}
+													onChange={(e) => myCheckboxFunction(e, d.id, index)}
+													disabled={functionToDisable(d.id, index)}
+												/>
+											)}
+											{e && (
+												<Checkbox
+													label={e.e}
+													onChange={(e) => myCheckboxFunction(e, e.id, index)}
+													disabled={functionToDisable(e.id, index)}
+												/>
+											)}
 										</div>
+									</div>
+								);
 							}
-						})
-					}
-					<button type={state.submited === true ? 'submit' : 'button'}  className='next' onClick={() => dispatch({ type: "NEXT" })}>{
-						state.slideIndex === 9 ? 'Submit' : 'Next'
-					}</button>
+						})}
+					<button
+						type={state.submited === true ? 'submit' : 'button'}
+						className='next'
+						onClick={() => dispatch({ type: 'NEXT' })}
+					>
+						{state.slideIndex === 9 ? 'Submit' : 'Next'}
+					</button>
+					{/* {getCount() && state.slideIndex === 9 && <button onClick={() => dispatch({ type: 'CHECK' })}>Start Again</button>} */}
 				</div>
 			</form>
-			{state.submited && <TransitionsModal opened={state.submited} count={count}/>}
+			{state.submited && (
+				<TransitionsModal opened={state.submited} count={count} />
+			)}
 		</div>
 	);
 };
