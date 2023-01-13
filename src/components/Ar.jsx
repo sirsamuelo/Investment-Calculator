9d411ec9c2a5088d7bae4d7ced8dbd8d322932f0
Samuel Sirotnik <samec070@gmail.com>
2023-01-13 12:31:14 +0100

    fixed expectedReturn and Intro css

--- src/components/Ar.jsx
+++ src/components/Ar.jsx
@@ -11,7 +11,7 @@ import Hiro from '../figma/Hiro.png';
 import Divider from '@mui/material/Divider';
 import Chip from '@mui/material/Chip';
 
-export default function Ar({finalAmount,duration,monthlyDeposit,expectedReturn,startingBalance}) {
+export default function Ar({data,finalAmount,duration,monthlyDeposit,expectedReturn,startingBalance}) {
 	return (
 		<Fragment>
 			<Divider sx={{ width: '80%', margin: '0 auto'}}>
@@ -20,7 +20,7 @@ export default function Ar({finalAmount,duration,monthlyDeposit,expectedReturn,s
 			<Typography variant='h6' gutterBottom sx={{margin: '2rem auto',width: '40%',textAlign: 'center'}}>
 				Hello my friend, I see you would like to invest <span>{startingBalance}€</span> at the
 				beggining of your journey. That`s awsome. Imagine, after <span>{duration}</span> years
-				and with your monthly deposit <span>{monthlyDeposit}€</span> and expected return <span>{expectedReturn}%</span> you can buy lots of things. Lets see what you can buy with your return <span>{finalAmount}€.</span>
+				and with your monthly deposit <span>{monthlyDeposit}€</span> and expected return <span>{data !== 0 ? data : expectedReturn}%</span> you can buy lots of things. Lets see what you can buy with your return <span>{finalAmount}€.</span>
 			</Typography>
 			<div className='card_container'>
 				<div className='card'>
